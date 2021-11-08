244dfb625d56f101c49948dc24bdfd45781fee51
Su Zhang <suzhang79@gmail.com>
2021-11-08 22:42:58 +1100

    clean up

--- javascript/functions.js
+++ javascript/functions.js
@@ -2,15 +2,6 @@ function press() {
     alert("no signal");
 }
 
-function quickAdd(x,y){
-    var answer=x+y;
-    return answer;
-}
-function test(){
-    var result=quickAdd(1,2);
-    alert(result);
-}
-
 function add(){
     var x = document.getElementById("number1").value;
     var y = document.getElementById("number2").value;
