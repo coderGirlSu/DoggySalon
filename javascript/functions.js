f40b2312f1bb957750fbc068e59ce28100623e8e
Su Zhang <suzhang79@gmail.com>
2021-11-08 22:37:48 +1100

    added an add function

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,19 @@
 function press() {
     alert("no signal");
 }
+
+function quickAdd(x,y){
+    var answer=x+y;
+    return answer;
+}
+function test(){
+    var result=quickAdd(1,2);
+    alert(result);
+}
+
+function add(){
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
+    var answer = parseInt(x)+ parseInt(y);
+    alert(answer);
+}
