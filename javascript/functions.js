89a43757093e0dfd107e5ed12796b2d757188256
Su Zhang <suzhang79@gmail.com>
2021-11-09 20:39:42 +1100

    Doggy Salon

--- javascript/functions.js
+++ javascript/functions.js
@@ -6,5 +6,12 @@ function add(){
     var x = document.getElementById("number1").value;
     var y = document.getElementById("number2").value;
     var answer = parseInt(x)+ parseInt(y);
-    alert(answer);
+    alert(answer); 
 }
+
+function times(){
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
+    var answer = parseInt(x) * parseInt(y); 
+    alert(answer);  
+ } 
